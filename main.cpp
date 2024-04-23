4b3d299ee84951750275cc1c82b1832d0a306ad6
Elijah <elijah.olson123@gmail.com>
2024-04-23 13:56:04 -0700

    menu1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
+#include <iostream>
 #include <algorithm>
 #include <vector>
 
 using namespace std;
+
+
+int main(){
+
+    int start = 0;
+    switch (start){
+        case 0:
+        cout << "------------------------\n";
+        cout << "| WELCOME TO TICTACTOE |\n";
+        cout << "------------------------\n";
+        cout << "|         TYPE:        |\n";
+        cout << "| 1) For the Rules     |\n";
+        cout << "| 2) To do X           |\n";
+        cout << "| 3) To do X           |\n";
+        cout << "| 4) To do X           |\n";
+        cout << "| 5) To do X           |\n";
+        cout << "------------------------\n";
+
+    }
+
+    return 0;
+}
